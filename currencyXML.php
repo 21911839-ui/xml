dd5d7386dceceb4165f1cf95192395adb1d01a6f
21911839-ui <21911839@students.hebron.edu>
2026-05-03 03:50:36 -0700

    Add files via upload

---
+++ currencyXML.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $i = 0 , $x = 0; 
+    
+    echo "<table border = '1'>
+           <tr> 
+              <th>Iso Code </th>
+              <th> Lib </th>
+              <th> Rate</th>
+            </tr>"; 
+    //.... 
+    if($x)
+    echo "$i Rows are inserted Successfully "; 
+    //...
+    ?>
+</body>
+</html>
